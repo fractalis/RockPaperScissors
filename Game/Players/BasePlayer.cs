7dfe17f1b9c6d99c4d998c699b16d46e8c8cfffd
Scott Rallya <srallya@gmail.com>
2020-11-07 08:08:26 -0500

    Added Scoring and Playing Multiple Rounds

--- Game/Players/BasePlayer.cs
+++ Game/Players/BasePlayer.cs
@@ -14,8 +14,12 @@ namespace RockPaperScissors.Game
 
         public string Name { get; set; }
 
+        public int Score { get; private set; }
+
         public IMoveType Move { get; internal set; }
 
         public abstract void GetMove();
+
+        public void IncrementScore() { Score += 1; }
     }
 }
